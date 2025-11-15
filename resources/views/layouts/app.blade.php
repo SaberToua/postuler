66032169a052bfdf75532a5ac58c381b5ef769d4
SaberToua <sabtoua18@gmail.com>
2025-11-15 20:23:01 +0100

    Replace Vite asset links with asset helper

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,8 @@
         @inertiaHead
         @viteReactRefresh
         @livewireStyles
-        @vite(['resources/css/app.css', 'resources/js/app.jsx'])
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+<script src="{{ asset('js/app.js') }}" defer></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     </head>
     <body class="font-sans antialiased dark:bg-slate-800 ">
